8b72e5ee1846f5df4f36b71045a30e9df2bd84e4
darren <13867270@student.uts.edu.au>
2021-11-01 20:20:14 +1100

    Revert "Merge branch 'master' into darren"
    This reverts commit 43f1c3651ce529fd3890de0b9e7ec391bbca6daf, reversing
    changes made to 6c6f90e8c91a6763031322f8478a69a940f790c3.

--- src/data.py
+++ src/data.py
@@ -6,95 +6,97 @@ import pandas as pd
 
 @dataclass
 class Dataset:
-	name: str = None
-	df: pd.DataFrame = None
+  name: str = None
+  df: pd.DataFrame = None
+  
+  def get_name(self):
+    """
+    Return filename of loaded dataset
+    """
+    self.name = self.df.columns.name
+    return None
 
-	def get_name(self):
-		"""
-		Return filename of loaded dataset
-		"""
-		return self.name
+  def get_n_rows(self):
+    """
+      Return number of rows of loaded dataset
+    """
+    n_rows = self.df.shape[0]
+    return n_rows
 
-	def get_n_rows(self):
-		"""
-			Return number of rows of loaded dataset
-		"""
-		n_rows = self.df.shape[0]
-		return n_rows
+  def get_n_cols(self):
+    """
+      Return number of columns of loaded dataset
+    """
+    n_cols = self.df.shape[1]
+    return n_cols
 
-	def get_n_cols(self):
-		"""
-			Return number of columns of loaded dataset
-		"""
-		n_cols = self.df.shape[1]
-		return n_cols
+  def get_cols_list(self):
+    """
+      Return list column names of loaded dataset
+    """
+    cols_list = self.df.columns.values
+    return cols_list
 
-	def get_cols_list(self):
-		"""
-			Return list column names of loaded dataset
-		"""
-		cols_list = self.df.columns.map(str).tolist()
-		return cols_list
+  def get_cols_dtype(self):
+    """
+      Return dictionary with column name as keys and data type as values
+    """
+    col_types = pd.DataFrame(self.df.dtypes).astype(str).to_dict()
+    return col_types 
 
-	def get_cols_dtype(self):
-		"""
-			Return dictionary with column name as keys and data type as values
-		"""
-		cols_dtype = self.df.dtypes.astype(str).to_dict()
-		return cols_dtype
+  def get_n_duplicates(self):
+    """
+      Return number of duplicated rows of loaded dataset
+    """
+    n_duplicates = sum(self.df.duplicated())
+    return n_duplicates
 
-	def get_n_duplicates(self):
-		"""
-			Return number of duplicated rows of loaded dataset
-		"""
-		n_duplicates = sum(self.df.duplicated())
-		return n_duplicates
+  def get_n_missing(self):
+    """
+      Return number of rows with missing values of loaded dataset
+    """
+    n_missing = self.df[self.df.isnull().any(axis=1)].shape[0]
+    return n_missing
 
-	def get_n_missing(self):
-		"""
-			Return number of rows with missing values of loaded dataset
-		"""
-		n_missing = sum(self.df.isnull().any(axis=1))
-		return n_missing
+  def get_head(self, n):
+    """
+      Return Pandas Dataframe with top rows of loaded dataset
+    """
+    head = self.df.head(n)
+    return head
 
-	def get_head(self, n):
-		"""
-			Return Pandas Dataframe with top rows of loaded dataset
-		"""
-		head = self.df.head(n)
-		return head
+  def get_tail(self, n):
+    """
+      Return Pandas Dataframe with bottom rows of loaded dataset
+    """
+    tail = self.df.tail(n)
+    return tail
 
-	def get_tail(self, n):
-		"""
-			Return Pandas Dataframe with bottom rows of loaded dataset
-		"""
-		tail = self.df.tail(n)
-		return tail
+  def get_sample(self, n):
+    """
+      Return Pandas Dataframe with random sampled rows of loaded dataset
+    """
+    sample = self.df.sample(n)
+    return sample
 
-	def get_sample(self, n):
-		"""
-			Return Pandas Dataframe with random sampled rows of loaded dataset
-		"""
-		sample = self.df.sample(n)
-		return sample
+  def get_numeric_columns(self):
+    """
+      Return list column names of numeric type from loaded dataset
+    """
+    numeric_columns = self.df.select_dtypes(include='number').columns.tolist()
+    return numeric_columns
 
-	def get_numeric_columns(self):
-		"""
-			Return list column names of numeric type from loaded dataset
-		"""
-		numeric_columns = self.df.select_dtypes(include='number').columns.tolist()
-		return numeric_columns
+  def get_text_columns(self):
+    """
+      Return list column names of text type from loaded dataset
+    """
+    text_columns = self.df.select_dtypes(include='object').columns.tolist()
+    return text_columns
 
-	def get_text_columns(self):
-		"""
-			Return list column names of text type from loaded dataset
-		"""
-		text_columns = self.df.select_dtypes(include='object').columns.tolist()
-		return text_columns
+  def get_date_columns(self):
+    """
+      Return list column names of datetime type from loaded dataset
+    """
+    date_columns = self.df.select_dtypes(include='datetime').columns.tolist()
+    return date_columns
 
-	def get_date_columns(self):
-		"""
-			Return list column names of datetime type from loaded dataset
-		"""
-		date_columns = self.df.select_dtypes(include=['datetime','timedelta']).columns.tolist()
-		return date_columns
